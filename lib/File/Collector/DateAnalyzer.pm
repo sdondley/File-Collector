e5b02d981a540ab6f761800fd82faf1534218f8c
Steve Dondley <s@dondley.com>
2019-08-30 10:21:10 -0400

    streamline creation of iterators

--- lib/File/Collector/DateAnalyzer.pm
+++ lib/File/Collector/DateAnalyzer.pm
@@ -3,20 +3,17 @@ package File::Collector::DateAnalyzer ;
 use strict; use warnings;
 use Log::Log4perl::Shortcuts qw(:all);
 use File::Collector::DateAnalyzer::Iterator;
-use File::Collector::DateAnalyzer::Bundle;
 use parent qw ( File::Collector );
 
 sub add_resources {
   my $s = shift;
 
   $s->SUPER::add_resources(@_);
-  my $iterator = ref($s) . '::Iterator';
-  $s->{files}{some_files}         = $iterator->new();
-  $s->{files}{other_files}        = $iterator->new();
-  $s->_test_blah;
+  $s->add_iterators( qw ( some_files other_files ) );
+  $s->_classify_files;
 }
 
-sub _test_blah {
+sub _classify_files {
   my $s = shift;
 
   foreach my $file ($s->get_files) {
